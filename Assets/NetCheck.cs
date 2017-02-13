2e772e43d6fd0e88e0f7723e5bc21197a40e6400
Rz Rasel <Rz Rasel>
2017-02-13 15:48:35 +0600

    ADD

---
+++ Assets/NetCheck.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetCheck : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Application.internetReachability == NetworkReachability.NotReachable) {
+			Debug.Log ("Error. Check internet connection!");
+		} else {
+			Debug.Log("Internet connected");
+		}
+	}
+}
